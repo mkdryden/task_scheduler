c00878f01e9d27200cc203f278685579cc440652
Christian Fobel <christian@fobel.net>
2014-02-07 08:22:48 -0500

    Restructure as Python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(name='task_scheduler',
+      version='0.0.1',
+      description='Schedule tasks based on requested relative partial '
+      'ordering.',
+      keywords='schedule task constraint',
+      author='Christian Fobel',
+      url='http://github.com/cfobel/task_scheduler.git',
+      license='GPL',
+      packages=['task_scheduler'])
